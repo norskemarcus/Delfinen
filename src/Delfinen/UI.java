c73b42be812fa9fc80fcd28ad1bb6f2f5597e1b1
tomm8231 <tomm8231@stud.kea.dk>
2022-05-11 11:24:28 +0200

    Invalid input in chairman's switch case handled

--- src/Delfinen/UI.java
+++ src/Delfinen/UI.java
@@ -6,8 +6,16 @@ public class UI {
   Scanner sc = new Scanner(System.in);
 
   public int inputNumber() {
-    System.out.println("Vælg et tal fra menuen:");
-    return sc.nextInt();
+
+    String numberString = sc.nextLine();
+    int number = 15;
+    try {
+      System.out.println("Vælg et tal fra menuen:");
+      number = Integer.parseInt(numberString);
+    } catch (NumberFormatException n) {
+      System.err.println("Kun tal");
+    }
+    return number;
   }
 
   public void showMenuOptionsChairman() {
@@ -16,5 +24,9 @@ public class UI {
     System.out.println("3: Vis medlemmer");
     System.out.println("4: Rediger medlemsoplysninger");
     System.out.println("5: Gå tilbage til hovedmenu");
+  }
+
+  public void errorMessage() {
+    System.out.println("Ugyldig indtastning - indtast et tal fra menuen.");
   }
 }
