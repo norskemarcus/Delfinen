0fcf6ed2dd703957268e6411ee533e7813f82ebf
norskemarcus <marc791j@stud.kea.dk>
2022-05-11 11:23:38 +0200

    added unit test for Competitor

--- src/Delfinen/MemberList.java
+++ src/Delfinen/MemberList.java
@@ -1,4 +1,22 @@
 package Delfinen;
 
+import java.util.ArrayList;
+
 public class MemberList {
+
+  private ArrayList<Member> allMembers;
+  private ArrayList<Competitor> allCompetitors;
+
+  public MemberList(){
+    allMembers = new ArrayList<>();
+    allCompetitors = new ArrayList<>();
+  }
+
+  public ArrayList<Member> getAllMembers() {
+    return allMembers;
+  }
+
+  public ArrayList<Competitor> getAllCompetitors() {
+    return allCompetitors;
+  }
 }
---
+++ test/Delfinen/CompetitorTest.java
@@ -0,0 +1,23 @@
+package Delfinen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompetitorTest {
+
+  @Test
+  void createNewMember(){
+    //arrange
+    Competitor sebastian = new Competitor("Sebastian", 20220001, 19, "seb@gmail.com", true, "M");
+    MemberList memberList = new MemberList();
+    //act
+    memberList.getAllMembers().add(sebastian);
+    memberList.getAllCompetitors().add(sebastian);
+    //assert
+    assertTrue(memberList.getAllMembers().size() == 1);
+    assertTrue(memberList.getAllCompetitors().size() == 1);
+
+  }
+
+}
