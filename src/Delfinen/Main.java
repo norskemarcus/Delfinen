318e95ba64fb8abc8049f4a34c614b5170f4ecc2
norskemarcus <marc791j@stud.kea.dk>
2022-05-12 10:26:19 +0200

    addNonCompetitorMember and addCompetitorMember

--- src/Delfinen/Main.java
+++ src/Delfinen/Main.java
@@ -3,6 +3,8 @@ package Delfinen;
 public class Main {
 
     public static void main(String[] args) {
+
+        //TODO: Husk i følge Klassediagrammet skal Main kun kende UI og UI skal kende ControllerMain
         ControllerMain mc = new ControllerMain();
         mc.run();
     }
