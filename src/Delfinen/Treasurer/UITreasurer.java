e7eed131b2d9b192308c5bffb3e261f58044ef21
tomm8231 <tomm8231@stud.kea.dk>
2022-05-17 11:40:25 +0200

    printExpectedAnnualSum added and functional - and something else

--- src/Delfinen/Treasurer/UITreasurer.java
+++ src/Delfinen/Treasurer/UITreasurer.java
@@ -10,7 +10,7 @@ public class UITreasurer extends UIMain implements PrintMenuInterface {
   private Scanner sc = new Scanner(System.in);
 
   public void printMenuOptions() {
-    System.out.println("1: Se forventeligt kontingentbudget - to be implemented");
+    System.out.println("1: Se forventeligt kontingentbudget");
     System.out.println("2: Medlemmer i restance - to be implemented");
     System.out.println("0: Gå tilbage til hovedmenu");
   }
